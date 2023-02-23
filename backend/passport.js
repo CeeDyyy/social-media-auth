20729658d5dcfe426d990ecbba32dcb90179e32c
CeeDyyy <thanawat.ud@mail.kmutt.ac.th>
2023-02-23 11:18:24 +0700

    key

--- backend/passport.js
+++ backend/passport.js
@@ -3,14 +3,14 @@ const GithubStrategy = require("passport-github2").Strategy;
 const FacebookStrategy = require("passport-facebook").Strategy;
 const passport = require("passport");
 
-const GOOGLE_CLIENT_ID = "730444868518-t8e9ginfpoc0e39dp0tt984ugv8c6qll.apps.googleusercontent.com"
-const GOOGLE_CLIENT_SECRET = "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"
+const GOOGLE_CLIENT_ID = "key"
+const GOOGLE_CLIENT_SECRET = "key"
 
-const GITHUB_CLIENT_ID = "0ce2aee50a3d6b491ef2"
-const GITHUB_CLIENT_SECRET = "53a33b9cdc6e4adbcea29635f32dfa7e1b660f63"
+const GITHUB_CLIENT_ID = "key"
+const GITHUB_CLIENT_SECRET = "key"
 
-const FACEBOOK_APP_ID = "0ce2aee50a3d6b491ef2"
-const FACEBOOK_APP_SECRET = "53a33b9cdc6e4adbcea29635f32dfa7e1b660f63"
+const FACEBOOK_APP_ID = "key"
+const FACEBOOK_APP_SECRET = "key"
 
 passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
